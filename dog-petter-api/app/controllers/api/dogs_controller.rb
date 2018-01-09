62ffa3c51e5e12430b77a60b7f07969526bf2ebe
Mel Chang <mel510@users.noreply.github.com>
2018-01-09 14:37:11 -0800

    Add more routes in DogController

--- dog-petter-api/app/controllers/api/dogs_controller.rb
+++ dog-petter-api/app/controllers/api/dogs_controller.rb
@@ -1,7 +1,46 @@
 class Api::DogsController < ApplicationController
+  before_action :current_dog, only: [:show, :update, :destroy]
 
   def index
     render json: Dog.all
   end
+
+  def show
+    render json: @dog
+  end
+
+  def create
+    dog = Dog.new(dog_params)
+    if dog.save
+      render json: dog
+    else
+      render json: { message: dog.errors}, status: 400
+    end
+  end
+
+  def update
+    if @dog.update(dog_params)
+      render json: @dog
+    else
+      render json: { message: @dog.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @dog.destroy
+      render status: 204
+    else
+      render json: { message: @dog.errors }, status: 400
+    end
+  end
+
+  private
+    def dog_params
+      params.require(:dog).permit(:name, :location, :image_url, :date, :breed)
+    end
+
+    def current_dog
+      @dog = Dog.find_by(id: params[:id])
+    end
 
 end
