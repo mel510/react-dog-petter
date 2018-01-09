f776e485085e45b6e843d3455dfd299e4fc2a434
Mel Chang <mel510@users.noreply.github.com>
2018-01-09 15:40:11 -0800

    Start DogCard and some CSS

--- dog-petter-client/src/containers/App.js
+++ dog-petter-client/src/containers/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Dogs from './Dogs'
 
+const dogs = [
+  {
+    name: "Pesto",
+    location: "Coffee shop",
+    image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/s640x640/sh0.08/e35/25005801_317666418733860_479226214595166208_n.jpg",
+    date: "01/01/18",
+    breed: "Lab"
+  },
+  {
+    name: "Lily",
+    location: "Dog park",
+    image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/s640x640/sh0.08/e35/24327267_135774497132218_710261799756234752_n.jpg",
+    date: "01/12/18",
+    breed: "Pit bull"
+  },
+  {
+    name: "Dory",
+    location: "Dog park",
+    image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/s640x640/sh0.08/e35/20969152_111474042898716_7644694581723791360_n.jpg",
+    date: "01/12/18",
+    breed: "German Shepherd"
+  },
+]
+
 
 
 class App extends Component {
--- dog-petter-client/src/containers/Dogs.js
+++ dog-petter-client/src/containers/Dogs.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Dogs.css'
 
-const Dogs = () =>
-  <div>
-    <h3>Dogs Component</h3>
-    <ul>
-    </ul>
+const Dogs = (props) =>
+  <div className="DogsContainer">
+    <h3>Dogs</h3>
+    {props.dogs.map(dog =>
+      <div className="DogCard">
+        <h4>{dog.name}</h4>
+        <p>{dog.date} &#124; {dog.location}</p>
+        <img className="DogImage" src={dog.image_url} alt={dog.name} />
+        <p>{dog.breed}</p>
+      </div>
+    )}
   </div>
 ;
 
