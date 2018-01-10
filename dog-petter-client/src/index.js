4ac4c42e5b66de1b2b8bec31dd06a711e1962347
Mel Chang <mel510@users.noreply.github.com>
2018-01-10 13:46:41 -0800

    Start Redux Provider

--- dog-petter-client/src/index.js
+++ dog-petter-client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js'
 
 ReactDOM.render(
-  <App store={store}/>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />,
+    document.getElementById('root')
+  </Provider>
 );
 
 registerServiceWorker();
