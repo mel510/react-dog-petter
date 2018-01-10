de7b55db280cf37658e458bf98600846b8c87714
Mel Chang <mel510@users.noreply.github.com>
2018-01-10 15:02:27 -0800

    Create form and form action

---
+++ dog-petter-client/src/actions/dogForm.js
@@ -0,0 +1,6 @@
+export const updateDogFormData = dogFormData => {
+  return {
+    type: 'UPDATED_DATA',
+    dogFormData
+  }
+}
--- dog-petter-client/src/containers/DogForm.js
+++ dog-petter-client/src/containers/DogForm.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { updateDogFormData } from '../actions/dogForm';
 
 class DogForm extends Component {
 
+  handleOnChange = event => {
+    const { name, value } = event.target;
+    const currentDogFormData = Object.assign({}, this.props.dogFormData, {
+      [name]: value
+    })
+    this.props.updateDogFormData(currentDogFormData)
+  }
+
   render() {
+    const { name, location, image_url, date, breed } = this.props.dogFormData;
     return(
       <div className="DogForm">
         <h3>Add a Dog</h3>
         <form>
+          <div className="FormField">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleOnChange}
+              value={name}
+            />
+          </div>
+          <div className="FormField">
+          <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              name="location"
+              onChange={this.handleOnChange}
+              value={location}
+            />
+          </div>
+          <div className="FormField">
+            <label htmlFor="image_url">Image URL:</label>
+            <input
+              type="text"
+              name="image_url"
+              onChange={this.handleOnChange}
+              value={image_url}
+            />
+          </div>
+          <div className="FormField">
+            <label htmlFor="datee">Date:</label>
+            <input
+              type="text"
+              name="date"
+              onChange={this.handleOnChange}
+              value={date}
+            />
+          </div>
+          <div className="FormField">
+            <label htmlFor="breed">Breed:</label>
+            <input
+              type="text"
+              name="breed"
+              onChange={this.handleOnChange}
+              value={breed}
+            />
+          </div>
         </form>
       </div>
     )
@@ -20,4 +75,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(DogForm);
+export default connect(mapStateToProps, { updateDogFormData })(DogForm);
