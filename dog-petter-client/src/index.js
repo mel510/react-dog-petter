702a01f19185149d533e13b40ddf10843cb522a6
Mel Chang <mel510@users.noreply.github.com>
2018-01-10 12:52:13 -0800

    File cleanup

--- dog-petter-client/src/index.js
+++ dog-petter-client/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store.js'
 
 ReactDOM.render(
-  <App />,
+  <App store={store}/>,
   document.getElementById('root')
 );
 
--- dog-petter-client/src/store.js
+++ dog-petter-client/src/store.js
@@ -1,2 +1,24 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+
+const dogsReducer = (state=[], action) => {
+  switch(action.type) {
+    case 'GET_DOGS_SUCCESS':
+      return action.dogs;
+
+    default:
+      return state;
+  }
+}
+
+const reducers = combineReducers({
+  dogs: dogsReducer
+});
+
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
