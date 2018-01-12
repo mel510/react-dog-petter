050a3e280a0112f1ec1e40c288801fb7edbf7e2b
Mel Chang <mel510@users.noreply.github.com>
2018-01-12 12:19:08 -0800

    Add Likes to Dog model and add seed data

--- dog-petter-api/db/schema.rb
+++ dog-petter-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109213419) do
+ActiveRecord::Schema.define(version: 20180112200556) do
 
   create_table "dogs", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180109213419) do
     t.string "breed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "like", default: 0
   end
 
 end
--- dog-petter-api/db/seeds.rb
+++ dog-petter-api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create(name: 'Pesto', location: 'Dog park', image_url: 'https://scontent-ort2-1.cdninstagram.com/vp/7eb884625edc15e0c89ce4cf8b046e87/5AF51991/t51.2885-15/s640x640/sh0.08/e35/25005801_317666418733860_479226214595166208_n.jpg', date: '01/01/18', breed: 'Lab')
+
+Dog.create(name: 'Lily', location: 'Coffee shop', image_url: 'https://scontent-ort2-1.cdninstagram.com/vp/adfe85a7d7f00cc965fa891296aed664/5AE94E7C/t51.2885-15/s640x640/sh0.08/e35/24327267_135774497132218_710261799756234752_n.jpg', date: '01/02/18', breed: 'Pit bull')
+
+Dog.create(name: 'Harley', location: 'Mosswood Park', image_url: 'https://scontent-ort2-1.cdninstagram.com/vp/d90851d38f96e5529dab53c76152b713/5AEDE473/t51.2885-15/s640x640/sh0.08/e35/25011410_525172794511164_8843473499394670592_n.jpg', date: '01/02/18', breed: 'German Shepherd')
