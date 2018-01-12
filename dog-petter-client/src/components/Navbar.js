05cb2a7e6087e91290a91fa1d20e02d05f8c38d4
Mel Chang <mel510@users.noreply.github.com>
2018-01-12 13:41:30 -0800

    Add nav styling

--- dog-petter-client/src/components/Navbar.js
+++ dog-petter-client/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="Nav">
       <ul>
-        <li><NavLink to='/' header>DogPetter</NavLink></li>
+        <li className="HomeItem"><NavLink to='/' header>DogPetter</NavLink></li>
         <li><NavLink to='/dogs' name='Dogs'>Dogs</NavLink></li>
         <li><NavLink to='/greeting-dogs' name='GreetingDogs'>Greeting Dogs</NavLink></li>
         <li><NavLink to='/about' name='About'>About</NavLink></li>
