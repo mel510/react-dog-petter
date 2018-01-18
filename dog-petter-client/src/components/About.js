7461d77f0757023a3bb4c033f844f106726a3e08
Mel Chang <mel510@users.noreply.github.com>
2018-01-17 17:38:08 -0800

    Add noopener noreferrer to external links

--- dog-petter-client/src/components/About.js
+++ dog-petter-client/src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='About'>
       <h1>About DogPetter</h1>
-      <p>DogPetter is a React/Redux frontend on a Rails backend app built for a Flatiron School project. <a href="https://github.com/mel510/react-dog-petter" target="_blank">Visit</a> the repo to learn more.</p>
+      <p>DogPetter is a React/Redux frontend on a Rails backend app built for a Flatiron School project. <a href="https://github.com/mel510/react-dog-petter" target="_blank" rel="noopener noreferrer">Visit</a> the repo to learn more.</p>
     </div>
   )
 }
--- dog-petter-client/src/components/GreetingDogs.js
+++ dog-petter-client/src/components/GreetingDogs.js
@@ -9,8 +9,8 @@ const GreetingDogs = () => {
       <p>Ask if you can take a photo then log your dog on DogPetter!</p>
       <br /><br />
       <p>More resources:</p>
-      <p><a href="http://www.cthumane.org/site/c.8qLKK1MELjI2F/b.8777371/k.2478/How_To_Safely_Greet_A_New_Dog.htm" target="_blank">How To Safely Greet A New Dog by the Connecticut Humane Society</a></p>
-      <p><a href="https://drsophiayin.com/blog/entry/how-to-greet-a-dog/" target="_blank">How To Greet A Dog by Sophia Yin, DVM, MS</a></p>
+      <p><a href="http://www.cthumane.org/site/c.8qLKK1MELjI2F/b.8777371/k.2478/How_To_Safely_Greet_A_New_Dog.htm" target="_blank" rel="noopener noreferrer">How To Safely Greet A New Dog by the Connecticut Humane Society</a></p>
+      <p><a href="https://drsophiayin.com/blog/entry/how-to-greet-a-dog/" target="_blank" rel="noopener noreferrer">How To Greet A Dog by Sophia Yin, DVM, MS</a></p>
     </div>
   )
 }
