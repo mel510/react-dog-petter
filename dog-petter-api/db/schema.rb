8133f01032821a822e86019a99cfaafc957f2341
Mel Chang <mel510@users.noreply.github.com>
2018-02-28 10:14:22 -0800

    Change from SQLite to Postgres

--- dog-petter-api/db/schema.rb
+++ dog-petter-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180112205734) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.string "location"
