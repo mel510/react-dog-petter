0a19de6db97789339d5fd4dd985fc62c4250198f
Mel Chang <mel510@users.noreply.github.com>
2018-02-28 10:17:41 -0800

    Add User model

--- dog-petter-api/db/schema.rb
+++ dog-petter-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112205734) do
+ActiveRecord::Schema.define(version: 20180228181614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180112205734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "like", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
